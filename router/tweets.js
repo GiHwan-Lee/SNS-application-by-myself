d5e8a00e8d864480889cda7a04f10094028f946a
Gi Hwan <ass302@naver.com>
2023-02-16 22:00:45 +0900

    'second'

--- router/tweets.js
+++ router/tweets.js
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 
-const tweets = [
+let tweets = [
   {
     id: "1",
     text: "안녕",
@@ -10,11 +10,18 @@ const tweets = [
     name: "bob",
     url: "https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg",
   },
+  {
+    id: "2",
+    text: "멋있으시네용",
+    createdAt: Date.now().toString(),
+    name: "kihwan",
+    username: "kihwan",
+  },
 ];
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get("/", (req, res) => {
   const username = req.query.username;
   const data = username
     ? tweets.filter((tweet) => tweet.username === username)
@@ -22,8 +29,51 @@ router.get("/", (req, res, next) => {
   res.status(200).json(data);
 });
 
-//이미 app.js에서 /tweets를 적어줬기 때문에 여기서는 따로 안 적어도 된다.
-//status가 아닌 statusCode를 넣으면 오류가 난다.
-//filter를 쓸거니깐 tweets를 배열로 만들어줘야 한다.
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const data = tweets.find((tweet) => tweet.id === id);
+  if (data) {
+    res.status(200).json(data);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post("/", (req, res) => {
+  const text = req.body.text;
+  const username = req.body.username;
+  const name = req.body.name;
+
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    username,
+    name,
+  };
+
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+
+router.put("/:id", (req, res) => {
+  const text = req.body.text;
+  const id = req.params.id;
+
+  const tweet = tweets.find((data) => data.id === id);
+
+  if (tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({ message: `${id} not found!!` });
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  tweets = tweets.filter((tweet) => tweet.id === id);
+  res.sendStatus(204);
+});
 
 export default router;
