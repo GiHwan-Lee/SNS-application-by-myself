602402ab67ba07fdc96385079acfadd0568ce567
Gi Hwan <ass302@naver.com>
2023-03-24 19:03:16 +0900

    'fifth'

--- router/tweets.js
+++ router/tweets.js
@@ -1,6 +1,16 @@
 import express from "express";
 import "express-async-errors";
+import { body } from "express-validator";
 import * as tweetController from "../controller/tweet.js";
+import { validate } from "../middleware/validator.js";
+
+const tweetValidate = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("please write at least 3 character"),
+  validate,
+];
 
 const router = express.Router();
 
@@ -8,9 +18,9 @@ router.get("/", tweetController.getTweets);
 
 router.get("/:id", tweetController.getTweet);
 
-router.post("/", tweetController.createTweet);
+router.post("/", tweetValidate, tweetController.createTweet);
 
-router.put("/:id", tweetController.updateTweet);
+router.put("/:id", tweetValidate, tweetController.updateTweet);
 
 router.delete("/:id", tweetController.deleteTweet);
 
